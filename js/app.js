7811e05ea354b57edaa3890deea2aaddf3216131
Steven Abrams <steven.abrams86@gmail.com>
2015-06-02 16:25:01 -0600

    working on style

--- js/app.js
+++ js/app.js
@@ -4,19 +4,19 @@ xhr.open('GET', 'http://www.reddit.com/subreddits.json', true);
 xhr.addEventListener('load', function(){
   var data = JSON.parse(xhr.response);
   var items = data.data.children;
-  var button = document.getElementsByClassName('button')[0];
   var div = document.body.getElementsByClassName('data')[0];
+  var head = document.body.getElementsByClassName('head')[0];
+  var heading = document.createElement('h1');
+  heading.innerHTML = "Reddit's hottest topics"
+  head.appendChild(heading);
     for(i=0; i < items.length; i++){
       var subr = items[i].data.display_name;
       var subrUrl = items[i].data.url;
-      console.log(subr, subrUrl);
       var a = document.createElement('a');
-      var p = document.createElement('p');
       a.href = "/show.html?subr="+subr+"&subrUrl="+subrUrl;
-      a.innerHTML= subr;
-      p.innerHTML= "";
+      a.innerHTML= "<div class='button'>"+subr+"</div>";
       div.appendChild(a);
-      div.appendChild(p);
+
     }
 });
 xhr.send(null);
--- js/show.js
+++ js/show.js
@@ -1,25 +1,24 @@
 var xhr = new XMLHttpRequest();
 var queryString = document.location.search;
 var query = parseQueryString(queryString);
-console.log(query);
+document.getElementsByTagName("title")[0].innerHTML = "Reddit Demo: "+query.subrUrl.substring(3, query.subrUrl.length-1);
 xhr.open('GET', 'http://www.reddit.com/'+query.subrUrl.substring(1)+'.json', true);
 xhr.addEventListener('load', function(){
   var data = JSON.parse(xhr.response);
   var items = data.data.children;
-  var button = document.getElementsByClassName('button')[0];
   var div = document.body.getElementsByClassName('data')[0];
+  var head = document.body.getElementsByClassName('head')[0];
+  var heading = document.createElement('h1');
+  heading.innerHTML = query.subrUrl.substring(3, query.subrUrl.length-1);
+  head.appendChild(heading);
     for(i=0; i < items.length; i++){
       var title = items[i].data.title;
       var url = items[i].data.url;
-      console.log(title, url);
       var a = document.createElement('a');
-      var p = document.createElement('p');
       a.href = url;
       a.target = "_blank";
-      a.innerHTML= title;
-      p.innerHTML= "";
+      a.innerHTML= "<div class='button'>"+title+"</div>";
       div.appendChild(a);
-      div.appendChild(p);
     }
 });
 xhr.send(null);
